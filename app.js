4d4f9debdd066eec62a58f416b169e9a962c7860
ethanryan <ethanryan@gmail.com>
2017-10-25 10:53:09 -0400

    added Procfile for Heroku

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ app.get('/', function(req, res) {res.status(200).send('Helloooooo World!'); });
 app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
+
+
+//everything above is for server
+app.post('/hello', function(req, res, next) {
+  var userName = req.body.user_name;
+  var botPayLoad = {
+    text: 'Hello ' + username +', welcome to the botplayground channel! Have fun :)'
+  };
+
+  if (username !== 'slackbot') {
+    return res.status(200).json(botPayLoad);
+  } else {
+    return res.status(200).end();
+  }
+});
